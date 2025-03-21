6e66486ec88c8aa9712c86b6f84ce518a8359447
codebycarter <kelvinjuma99@gmail.com>
2025-03-21 18:22:50 +0300

    API fetch request to retrieve images in the db database

--- src/pages/List.jsx
+++ src/pages/List.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { backendUrl } from "../App";
 
 const List = () => {
+  const [list, setList] = useState([]);
+
+  const fetchList = async () => {
+    try {
+      const response = await axios.get(backendUrl + "/api/product/list");
+      console.log(response);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    fetchList();
+  }, []);
   return <div></div>;
 };
 
